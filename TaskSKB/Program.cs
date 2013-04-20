d860998c062e3a1852d7625d1fafc8d80f31adce
Sofia Nikolaenko <sofia-nikolaenko@yandex.ru>
2013-04-20 16:12:45 +0700

    True Initial Commit
    Без ориентации

---
+++ TaskSKB/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TaskSKB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = new Task();
+
+            var str = Console.ReadLine();
+
+            Console.WriteLine(str);
+
+            int a;
+
+            if (Int32.TryParse(str, out a))
+                Console.WriteLine(a.ToString());
+            else
+                Console.WriteLine("fail parse");
+        }
+    }
+}
